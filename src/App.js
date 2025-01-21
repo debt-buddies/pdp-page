15167b3dce6057bde99999f64e5f8750bd415535
KevinFalah <kevin.ghossan@mandalafinance.com>
2025-01-21 16:24:07 +0700

    add detail page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React, { lazy, Suspense } from "react";
+import React, { useState } from "react";
 import "./App.scss";
-
-const Header = lazy(() => import("HomeApp/Header"));
-// import Footer from "./Footer";
+import useStore from "HomeApp/hooks/useStore";
+import { useStoreSelector } from "HomeApp/hooks/useStoreSelector";
 
 function App() {
   return (
     <div className="main">
-      <Suspense fallback={<div>loading header...</div>}>
-        <Header subtitle="for main" bgColor="grey" />
-      </Suspense>
-      <div className="mainContent">Ini main nya loh</div>
+      <div className="mainContent">
+        <MainContent />
+      </div>
     </div>
   );
 }
+
+const MainContent = () => {
+  const {
+    todos: { previewContent, listTodos },
+  } = useStoreSelector((state) => state);
+
+  const { addTodos } = useStore();
+
+  const [text, setText] = useState("");
+
+  console.log(listTodos);
+  const handleSubmit = (e) => {
+    if (e.key === "Enter") {
+      addTodos(text);
+      setText("");
+    }
+  };
+
+  if (previewContent === "INPUT") {
+    return (
+      <div className="input-container">
+        <span>Silahkan isi disini</span>
+        <input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={handleSubmit}
+        />
+      </div>
+    );
+  }
+
+  return <div className="input-container">apa aja untuk tes aja ini mah!!!</div>;
+};
 
 export default App;
